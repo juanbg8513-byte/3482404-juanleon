494455eec0bd8db80c47b358ba9c713339c16f34
andres777-b <omgandrex@gmail.com>
2026-08-04 14:31:27 -0500

    Ejercicio 2 — Promedio y estado del curso

---
+++ src/ejercicio2.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(
+                JOptionPane.showInputDialog("¿Cuántos estudiantes hay?")
+        );
+
+        double[] notas = new double[n];
+
+        double suma = 0;
+
+        for (int i = 0; i < n; i++) {
+            notas[i] = Double.parseDouble(
+                    JOptionPane.showInputDialog(
+                            "Ingrese la nota del estudiante " + (i + 1)
+                    )
+            );
+
+            suma += notas[i];
+        }
+        double promedio = suma / n;
+
+        String resultado;
+
+        if (promedio >= 3.0) {
+            resultado = "Aprobado";
+        } else {
+            resultado = "Reprobado";
+        }
+
+        JOptionPane.showMessageDialog(
+                null,
+                "Promedio del curso: " + promedio +
+                        "\nResultado: " + resultado
+        );
+    }
+
+}
