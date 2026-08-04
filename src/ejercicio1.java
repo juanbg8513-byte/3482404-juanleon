75f81b47dca3f916a72de7652c69cb16677ee1cc
andres777-b <omgandrex@gmail.com>
2026-08-04 14:03:26 -0500

    Agrego carpeta src con Main.java

---
+++ src/ejercicio1.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+
+        int Cnumeros = 0;
+        int pares = 0;
+        int impar = 0;
+
+        Cnumeros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de numeros deseada "));
+
+        int[] numeros = new int[Cnumeros];
+
+        for (int i = 0; i < Cnumeros; i++) {
+
+            numeros[i]=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero:" + (i+1)));
+
+        }
+
+        for (int i=0; i<Cnumeros;i++){
+
+            if(numeros)
+
+
+        }
+
+
+
+    }
+
+}
